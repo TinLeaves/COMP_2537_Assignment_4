e7996aea5cfcbefcd557bb2b379c4b7f4688e9c4
Steven Lai <tinleaves.0@gmail.com>
2023-05-23 22:38:44 -0700

    fixed styling issue and added timer

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ let numClicks = 0;
 let pairsLeft;
 let powerUpActive = false;
 let isGameStarted = false;
+let timerInterval;
 
 function flipCard() {
   if (powerUpActive || !isGameStarted) return; // Prevent flipping cards during power-up or before the game starts
 
   if (this === firstCard) return; // Corner case 1: If the user clicks on the same card twice, do nothing
-  
+
   if (this.classList.contains('matched')) return; // Corner case 2: If the user clicks on a card that is already matched, do nothing
-  
+
   if (secondCard) return; // Corner case 3: If the user clicks on a card while two cards are already flipped, do nothing
 
   this.classList.add('flip');
@@ -82,10 +83,10 @@ function updateGameStats() {
   document.getElementById('clicks').textContent = numClicks;
 }
 
-function endGame() {
+function endGame(message = 'Congratulations! You have completed the game!') {
   clearInterval(timerInterval);
   setTimeout(() => {
-    alert('Congratulations! You have completed the game!');
+    alert(message);
   }, 500);
 }
 
@@ -95,23 +96,19 @@ function fetchRandomPokemon(difficulty) {
     gridRows = 2;
     gridColumns = 3;
     totalPairs = 3;
-    timeLimit = 100;
   } else if (difficulty === 'medium') {
     gridRows = 3;
     gridColumns = 4;
     totalPairs = 6;
-    timeLimit = 200;
   } else if (difficulty === 'hard') {
     gridRows = 4;
     gridColumns = 6;
     totalPairs = 12;
-    timeLimit = 300;
   } else {
     // Default to easy mode if no difficulty is provided
     gridRows = 2;
     gridColumns = 3;
     totalPairs = 3;
-    timeLimit = 100;
   }
 
   const totalCards = gridRows * gridColumns;
@@ -161,23 +158,19 @@ function generateGameGrid(difficulty) {
     gridRows = 2;
     gridColumns = 3;
     totalPairs = 3;
-    timeLimit = 100;
   } else if (difficulty === 'medium') {
     gridRows = 3;
     gridColumns = 4;
     totalPairs = 6;
-    timeLimit = 200;
   } else if (difficulty === 'hard') {
     gridRows = 4;
     gridColumns = 6;
     totalPairs = 12;
-    timeLimit = 300;
   } else {
     // Default to easy mode if no difficulty is provided
     gridRows = 2;
     gridColumns = 3;
     totalPairs = 3;
-    timeLimit = 100;
   }
 
   const totalCards = gridRows * gridColumns;
@@ -231,11 +224,51 @@ function startGame() {
   numClicks = 0;
   isGameStarted = true;
   updateGameStats();
+
+  const timeLimit = getTimeLimit(); // Get the time limit based on the selected difficulty
+  let timeLeft = timeLimit;
+
+  updateTimerDisplay(timeLeft); // Display initial time
+
+  timerInterval = setInterval(() => {
+    timeLeft--;
+
+    if (timeLeft <= 0) {
+      clearInterval(timerInterval);
+      endGame('Time is up! Game over!');
+    }
+
+    updateTimerDisplay(timeLeft);
+  }, 1000);
 }
 
 // Function to handle start button click
 function handleStartButtonClick() {
   startGame();
+}
+
+// Function to update the timer display
+function updateTimerDisplay(timeLeft) {
+  const timerDisplay = document.getElementById('timer');
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+  timerDisplay.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
+
+// Function to get the time limit based on the selected difficulty
+function getTimeLimit() {
+  const selectedDifficulty = document.querySelector('#difficulty input:checked').value;
+
+  if (selectedDifficulty === 'easy') {
+    return 100;
+  } else if (selectedDifficulty === 'medium') {
+    return 200;
+  } else if (selectedDifficulty === 'hard') {
+    return 300;
+  } else {
+    // Default to easy mode if no difficulty is provided
+    return 100;
+  }
 }
 
 // Call the handleDifficultySelection function when the page loads
